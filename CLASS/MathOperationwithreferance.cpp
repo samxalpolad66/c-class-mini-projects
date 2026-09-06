348a96ad69fbb1fb9a1a0d8ba88d4e063981c06f
samxalpolad66 <samxalpolad666@gmail.com>
2026-09-06 17:21:36 +0400

    Add files via upload

---
+++ CLASS/MathOperationwithreferance.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class MathOperations{
+    private:
+    int result;
+    public:
+    MathOperations(int result){
+        this -> result = result;
+    }
+    MathOperations& add(int val){
+        result = result + val;
+        return *this;
+    }
+    MathOperations& subtract(int val){
+        result = result - val;
+        return *this;
+    }
+    MathOperations& multiply(int val){
+        result = result * val;
+        return *this;
+    }
+    MathOperations& divide(int val){
+        if(val != 0){
+            result = result / val;
+        }
+        else{
+            cout << "Invalid" <<endl;
+        }
+        return *this;
+    }
+    int getResult(){
+        return result;
+    }
+};
+int main() {
+    system("cls");
+
+    /*      Example usage:     */
+    MathOperations math(10);
+    
+    math.add(5).subtract(2).multiply(3).divide(4);
+    
+    cout << "Final result: " << math.getResult() << endl;
+    
+    return 0;
+}
